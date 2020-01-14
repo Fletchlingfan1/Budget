264deb2ccdca70051913685beba230d5deff4805
Daxton Dollar <daxdollar9@gmail.com>
2020-01-14 08:43:35 -0700

    no message

---
+++ Budget/Budget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Budget
+//
+//  Created by Daxton Dollar on 1/14/20.
+//  Copyright © 2020 Daxton Dollar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
