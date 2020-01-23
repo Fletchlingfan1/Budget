ba7f1e87bc9ab542bcf522f3bbe229e492ce7f42
Jordy <jordyvazgol@gmail.com>
2020-01-23 09:55:27 -0700

    CoreData

---
+++ Budget/Model/Budget.swift
@@ -0,0 +1,23 @@
+//
+//  Budget.swift
+//  Budget
+//
+//  Created by Jordy Vazquez on 1/23/20.
+//  Copyright © 2020 Daxton Dollar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Budget {
+    convenience init?(budgetName: String, budgetAmount: Double, budgetTotal:Double, date:Date, context: NSManagedObjectContext = Stack.context){
+        
+        self.init(context:context)
+        
+        self.budgetName = budgetName
+        self.budgetAmount = budgetAmount
+        self.budgetTotal = budgetTotal
+        self.date = date
+        
+    }
+}
