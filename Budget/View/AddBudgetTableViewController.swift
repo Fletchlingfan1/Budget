6528260e64e8e3401cf77a6a22cd67eaa26f626f
Jacob Mower <jhmower@gmail.com>
2020-01-27 09:58:47 -0700

    story board removal stuff

--- Budget/View/AddBudgetTableViewController.swift
+++ Budget/View/AddBudgetTableViewController.swift
@@ -24,9 +24,13 @@ class AddBudgetTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 3
     }
 
+    @IBAction func saveButton(_ sender: Any) {
+        
+//        BudgetController.sharedController.saveBudget
+    }
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
